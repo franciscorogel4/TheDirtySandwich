a8478d9d5666b8a62c33df81755355b0927e776c
Willem Beeson <wbeeson@villanova.edu>
2017-10-05 23:27:44 -0400

    added firebase library

--- willem-wf3/Main.js
+++ willem-wf3/Main.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactNative from 'react-native';
+import * as firebase from 'firebase';
 import { StyleSheet, Text, View, FlatList, StatusBar, Alert } from 'react-native';
 import { SearchBar } from 'react-native-elements';
 import { createFilteredArray } from './custom_components/SearchFilter';
@@ -8,14 +10,23 @@ let pic = {
   uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
 };
 
-var cells = [{title: 'A', description: 'B', source: pic, key: 'a'}, {title: 'A', description: 'D', source: pic, key: 'b'},
-{title: 'E', description: 'F', source: pic, key: 'c'}, {title: 'G', description: 'H', source: pic, key: 'd'},
-{title: 'I', description: 'A', source: pic, key: 'e'}, {title: 'A', description: 'L', source: pic, key: 'f'},
-{title: 'M', description: 'N', source: pic, key: 'g'}, {title: 'O', description: 'P', source: pic, key: 'h'},
-{title: 'Q', description: 'A', source: pic, key: 'i'}, {title: 'A', description: 'T', source: pic, key: 'j'},
-{title: 'U', description: 'A', source: pic, key: 'k'}];
+var cells = [{title: 'A Chair', description: 'B', source: pic, key: 'a'}, {title: 'Three Chairs', description: 'D', source: pic, key: 'b'},
+{title: 'Couch', description: 'F', source: pic, key: 'c'}, {title: 'Textbook', description: 'H', source: pic, key: 'd'},
+{title: 'Math textbook', description: 'A', source: pic, key: 'e'}, {title: 'physics textbook', description: 'L', source: pic, key: 'f'},
+{title: 'HISTORY TEXTBOOK', description: 'N', source: pic, key: 'g'}, {title: 'Lamps', description: 'P', source: pic, key: 'h'},
+{title: 'Bed', description: 'A', source: pic, key: 'i'}, {title: 'TV', description: 'T', source: pic, key: 'j'},
+{title: 'Toothbrush', description: 'A', source: pic, key: 'k'}];
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDBXjNByBcC2K5fBgK-hTrqNhhjOR3fKgw',
+  authDomain: 'novaemporium-5b87b.firebaseapp.com',
+  databaseURL: 'https://novaemporium-5b87b.firebaseio.com',
+  storageBucket: 'novaemporium-5b87b.appspot.com'
+};
+firebase.initializeApp(firebaseConfig);
 
 export default class Main extends React.Component {
+
 
   constructor(props){
     super(props);
--- willem-wf3/custom_components/SearchFilter.js
+++ willem-wf3/custom_components/SearchFilter.js
@@ -3,13 +3,15 @@ import React from 'react';
 export function createFilteredArray(filterText, rawArray){
 
   var filteredArray = [];
+  var filterTextRegEx = new RegExp(filterText, 'i');
+  var regExArray = [filterTextRegEx];
 
   if(filterText == ''){
     return rawArray;
   }
 
-  for (var cellData of rawArray){
-    if(cellData.title.toLowerCase() == filterText || cellData.description.toLowerCase() == filterText){
+  for(var cellData of rawArray){
+    if(cellData.title.toLowerCase().match(filterTextRegEx) || cellData.description.toLowerCase().match(filterTextRegEx)){
       filteredArray.push(cellData);
     }
   }
